7ae090f69ef32d6885c4c107cec2ae09cd78fae2
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-21 11:43:55 +0100

    Added the `Booking` api

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Booking, Menu
 
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['title', 'price', 'inventory']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
 def index(request):
@@ -16,3 +17,8 @@ class MenuItemsView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
